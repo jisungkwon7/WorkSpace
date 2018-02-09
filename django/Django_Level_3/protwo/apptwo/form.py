580d78fb3ca818cf77d304d45dab407ed38d8cb6
sse <jisungkwon7@gmail.com>
2018-02-09 10:32:50 +0900

    Feb09th2018

--- django/Django_Level_3/protwo/apptwo/form.py
+++ django/Django_Level_3/protwo/apptwo/form.py
@@ -1,7 +1,7 @@
-from django import fomrs
+from django import forms
 from .models import User
 
-class NewUser(forms.ModelForm):
+class NewUserForm(forms.ModelForm):
     class Meta():
         model = User
         fields = '__all__'
--- django/Django_Level_3/protwo/apptwo/views.py
+++ django/Django_Level_3/protwo/apptwo/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from apptwo.models import User
+# from django.http import HttpResponse
+# from apptwo.models import User
+from apptwo.form import NewUserForm
 
 def index(request):
     return render(request,'apptwo/index.html')
 
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {'users':user_list}
-    return render(request,'apptwo/users.html',context=user_dict)
+
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('ERROR FROM INVALID')
+
+    return render(request,'apptwo/users.html',{'form':form})
