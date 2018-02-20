9d54459197bcd5609af903c405fb061ddf546195
Jisung Kwon <jisungkwon7@gmail.com>
2018-02-21 01:36:36 +0900

    Feb21st2018

--- django/datumrefinery/src/profiles/views.py
+++ django/datumrefinery/src/profiles/views.py
@@ -5,3 +5,18 @@ def index(request):
     context ={}
     template = 'index.html'
     return render(request,template,context)
+
+def about(request):
+    context ={}
+    template = 'about.html'
+    return render(request,template,context)
+
+def worldpopulation(request):
+    context ={}
+    template = 'population/world.html'
+    return render(request,template,context)
+
+def koreanpopulation(request):
+    context ={}
+    template = 'population/korea.html'
+    return render(request,template,context)
